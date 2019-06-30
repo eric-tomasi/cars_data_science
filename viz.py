76eac01d90842de89cf909c74b766c1636caa6ce
Eric Tomasi <etomasi2323@gmail.com>
2019-06-30 16:43:59 -0400

    feat: added plot_trend func

--- viz.py
+++ viz.py
@@ -39,3 +39,24 @@ def clean_data():
 	df.drop(['price2', 'mileage2'], 1, inplace=True)
 
 	return df
+
+
+def plot_trend(df):
+
+	#set up figure and alter settings
+    fig, ax1 = plt.subplots(figsize=(20,8.5))
+    plt.rcParams.update({'font.size':14})
+    sns.set()
+
+    #plot smoothed actuals and regression with upper and lower bounds
+    ax1.plot(df['mileage'], df['price'], color='xkcd:cobalt', label='Price', linewidth=3.5)
+    ax1.set_xlabel('Mileage')
+    ax1.set_ylabel('Price')
+
+    #set title, legened, and savefig
+    plt.title('Price by mileage')
+    plt.legend(loc='lower center')
+    fig.savefig('trend.png', dpi=fig.dpi)
+
+
+plot_trend(clean_data())
