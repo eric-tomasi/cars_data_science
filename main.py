94500227afa4e8b5add8604df30ab701fc3300b3
Eric Tomasi <etomasi2323@gmail.com>
2019-07-03 14:17:35 -0400

    feat: implemented main function to run entire process

---
+++ main.py
@@ -0,0 +1,10 @@
+from scrape import run_scraper
+from viz import clean_data, plot_trend
+
+
+def main():
+	'''main function which executes cars.com scraper, writes data to a csv, cleans it, and plots trend with a fitted curve'''
+
+	run_scraper()
+
+	plot_trend(clean_data())
